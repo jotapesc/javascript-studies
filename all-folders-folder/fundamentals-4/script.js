3227951baf6c6a0bc98434529280fdcc225acd1b
Akarui <minezicabr0@gmail.com>
2025-03-20 20:44:52 -0300

    feat(subjects): created an array
    created an array to practice how to find an element in an array in javascript

--- all-folders-folder/fundamentals-4/script.js
+++ all-folders-folder/fundamentals-4/script.js
@@ -6,7 +6,7 @@ numbers.unshift(0);
 console.log(numbers);
 
 // Middle (splice)
-numbers.splice(2, 0, 1.5); //(index, who will remove after that index, who will add)
+numbers.splice(2, 0, 1.5); //(index, who it will remove after that index, who it will add)
 console.log(numbers);
 
 // End (push)
@@ -14,3 +14,21 @@ numbers.push(4);
 console.log(numbers);
 
 console.log("==================================");
+
+// Finding elements (primitive type)
+const letters = ["a", "b", "c", "d"];
+console.log(letters.indexOf("c")); // this returns the index
+console.log(letters.includes("c")); // this searchs if the element is present or not in the array
+
+// Finding elements (reference type)
+const brands = [
+    {brandName: "name1", id: 10},
+    {brandName: "name2", id: 15},
+    {brandName: "name3", id: 20},
+];
+
+function evenID(brands) {
+    return brands.id % 2 === 0;
+}
+
+console.log(brands.find(evenID));
